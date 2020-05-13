d001463ac08d1b2e642214f67e9a45df0fc621b4
Preto115 <preto115@gmail.com>
2020-05-13 11:57:27 -0300

    implementacao do exercicio_3

---
+++ exercises/4_1/exercise_3.js
@@ -0,0 +1,12 @@
+// programa que compara 3 numeros e fala qual o maior deles do exercicio_3
+
+let a = 20;
+let b = 130;
+let c = 12;
+let maior;
+
+( a > b) ? maior = a : maior =b;
+( c > maior ) ? maior = c : maior;
+
+
+console.log("o maior numero entre os tres numero comparado  é : " + `maior`);
