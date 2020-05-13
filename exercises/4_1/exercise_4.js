c7bd29f23b10cb1a087cd13d0e22d9278e5eedad
Preto115 <preto115@gmail.com>
2020-05-13 12:21:10 -0300

    criacao e implementacao do exercicio_4

---
+++ exercises/4_1/exercise_4.js
@@ -0,0 +1,7 @@
+// programa que verifica dse um numero é positivo, negativo ou zero e retorna o resultadodo exercicio_4
+let num = 4;
+if (num == 0){
+
+console.log("o numero especificado é zero!");
+}
+else console.log(( num > 0 ) ? " o numero é positivo" : " o numero é negativo");
