a6c653655f1417cadd4b87cf615ad5fc952e1f56
preto115 <preto115@gmail.com>
2020-05-16 14:28:58 -0300

    adicionando nova propriedade ao objeto

---
+++ exercises/4_4/exer_parte_1-objetos.js
@@ -0,0 +1,11 @@
+//bloco 4_4
+//exercicio 1, acessando propriedades dentro de um objeto
+
+ let info = {
+personagem: "Margarida",
+origem: "Pato Donald",
+nota: "Namorada do personagem do principal nos quadrinhos do Pato Donald"
+};
+
+info.recorrente = "Sim";
+console.log(info);
---
+++ exercises/4_4/exercicio_1.js
@@ -0,0 +1,10 @@
+//bloco 4_4
+//exercicio 1, acessando propriedades dentro de um objeto
+
+ let info = {
+personagem: "Margarida",
+origem: "Pato Donald",
+not: "Namorada do personagem do principal nos quadrinhos do Pato Donald"
+};
+
+console.log("Bem-vinda, ",info.personagem);
