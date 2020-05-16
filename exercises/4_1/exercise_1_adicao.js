65e0bbd6221c0c6e0ccd772b97cfeeb9bdc2e332
preto115 <preto115@gmail.com>
2020-05-16 13:44:41 -0300

    criando e refatorando exercicios parte_1 do 4_1

--- exercises/4_1/exercise_1_adicao.js
+++ exercises/4_1/exercise_1_adicao.js
@@ -1,3 +1,4 @@
 // exercicio_1 programa de adição
 let a = 10, b = 10;
+let soma =0;
 console.log( a + b);
---
+++ exercises/4_4/refatorando_codigo.js
@@ -0,0 +1,71 @@
+//bloco 4.4
+// programa feito para refatorar a primeira parte dos exercicios do bloco 4.1 e implementa-los como funcoes.
+
+let num1 = 4, num2 = 15;
+
+console.log(`A soma de ${num1} e ${num2} é:`,soma( num1, num2));
+console.log(`A subtracao de ${num1} e ${num2} é:`,subtracao( num1, num2));
+console.log(`A multiplicação de ${num1} por ${num2} é:`,multiplicacao( num1, num2));
+console.log(`A divisao de ${num1} por ${num2} é:`,divisao( num1, num2));
+console.log(`O módulo de ${num1} por ${num2} é:`,modulo( num1, num2));
+console.log(`A exponenciação de ${num1} elevado á ${num2} é:`,exponenciacao( num1, num2));
+
+// FUNÇÕES IMPLEMENTADAS.
+
+// programa de exponenciação do exercicio_1 
+function exponenciacao( a, b){
+   let exponenciacao = 0;
+   exponenciacao = num1 ** num2;
+   return exponenciacao;
+}
+
+
+// programa de modulo do exercicio_1 
+function modulo( a, b){
+
+    let modulo = "";
+    if ( num1 > num2){
+    modulo = num1 % num2;
+}
+   else modulo = "módulo não válido";
+    return modulo;
+
+}
+
+
+// programa de divisao do exercicio_1 
+function divisao( a, b){
+
+    let divisao = 0;
+    divisao = a / b;
+    return divisao;
+}
+
+
+// programa de multiplicacao do exercicio_1 
+
+function multiplicacao( a, b){
+    let multiplicacao =0;
+    
+     multiplicacao = a * b;
+    return multiplicacao;
+    }
+
+
+// exercicio_1 programa de adição
+
+function soma( a, b){
+let soma =0;
+soma = a + b;
+return soma;
+}
+
+
+// programa de subtração do exercicio_1 
+function subtracao( a, b){
+let subtracao =0;
+
+subtracao = a - b;
+return subtracao;
+
+}
