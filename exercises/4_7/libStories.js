4e36cbb2559f422c93a1328a439a4f1d1ffa49a3
preto115 <preto115@gmail.com>
2020-05-20 20:09:35 -0300

    testa de novo ai

--- exercises/4_7/libStories.js
+++ exercises/4_7/libStories.js
@@ -1,6 +1,6 @@
 let botao = document.querySelector("#lib-button");
 let historia = botao.addEventListener('click', makeMadLib);
-document.querySelector("#story") = historia;
+document.querySelector("#story").innerHTML = historia;
 
 function makeMadLib(){
 let noun = document.querySelector("#noun").value;
@@ -8,6 +8,7 @@ let adj = document.querySelector("#adjective").value;
 let name = document.querySelector("#person").value;
 
 let texto = name + " gostava muito do seu " + noun+" "+adj;
+document.querySelector("#story").innerHTML = texto;
 return texto;
 
 }
