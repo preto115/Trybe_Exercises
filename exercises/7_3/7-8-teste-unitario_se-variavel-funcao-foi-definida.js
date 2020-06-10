5e28201721a37737260af7829c2f983d299f70de
preto115 <preto115@gmail.com>
2020-06-10 18:39:52 -0300

    exercicios todos incompletos

---
+++ exercises/7_3/7-8-teste-unitario_se-variavel-funcao-foi-definida.js
@@ -0,0 +1,14 @@
+//7. Teste se uma variável foi definida
+const assert = require('assert');
+
+const thereIs = '';
+
+// implemente seus testes aqui
+
+
+//8. Teste se uma função foi definida
+const assert = require('assert');
+
+function isThere() {}
+
+// implemente seus testes aqui
