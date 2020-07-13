886318b98c9e4d7a5051cfd38502a32a1a76d752
Preto115 <preto115@gmail.com>
2020-07-13 14:52:54 -0300

    exercicios pre-aula_feito

---
+++ exercises/12_1/bloco-12_1/src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Botao extends React.Component{
+  render () {
+  return (<button onClick={() => this.props.handleClick(this.props.btn)}>{this.props.btn}</button>);
+  }
+}
+
+class Texto extends Component{
+  render () {
+    return(<span>Oi, como vai? Você clicou no {this.props.text}</span>);
+  }
+}
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      texto: 'teste',
+      outroTexto: 'botao'
+    }
+  }
+
+  mudaEstado = texto => this.setState({texto});
+
+  render() {
+    console.log(this.state)
+
+    return (
+      <div className="App App-header">
+      
+        <Texto text={this.state.texto}/>
+        <Botao handleClick={this.mudaEstado} btn='botao 1'/>
+        <Botao handleClick={this.mudaEstado} btn='botao 2'/>
+        <Botao handleClick={this.mudaEstado} btn='botao 3'/>
+        <Botao handleClick={this.mudaEstado} btn='botao 4'/>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ exercises/12_1/bloco-12_1/src/exercicios-pre_aula.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-DOM';
+
+class Botao extends React.Component{
+  render () {
+  return (<button>botao</button>);
+  }
+}
+ReactDOM.render(<Botao />, document.getElementById('root'));
