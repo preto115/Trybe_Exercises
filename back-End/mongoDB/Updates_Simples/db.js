c79f9a863195a547f87d759b23772fe5c9508ef6
Preto115 <preto115@gmail.com>
2020-10-26 22:11:21 -0300

    banco class collection movies

---
+++ back-End/mongoDB/Updates_Simples/db.js
@@ -0,0 +1,20 @@
+{
+  "title": "Batman",
+  "category": [ "action", "adventure" ],
+  "imdbRating": 7.6,
+  "budget": 35
+},
+{
+  "title": "Godzilla",
+  "category": [ "action", "adventure", "sci-fi" ],
+  "imdbRating": 6.6
+},
+{
+  "title": "Home Alone",
+  "category": [ "family", "comedy" ],
+  "imdbRating": 7.4
+}
+//  for i in {1..13}
+  //   do
+  //     mv back-End/mongoDB/Updates_Simples/"desafio${i}.js" back-End/mongoDB/Updates_Simples/"exercicio${i}.js"
+  //   done
